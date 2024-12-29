b400c7addff15b31ab20491e9e632c319e30cd47
CampanaHD <gabriel.campana005@gmail.com>
2024-12-29 18:18:45 -0300

    add swiper 2 e rodape

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-new Swiper('.card-wrapper', {
+const swiper1 = new Swiper('.mySwiper1', {
     loop: true,
   
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+      el: '.btn-pagination-1',
       clickable: true,
       
     },
   
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.btn-next-1',
+      prevEl: '.btn-prev-1',
     },
 
     spaceBetween: 10,
@@ -21,3 +21,29 @@ new Swiper('.card-wrapper', {
     slidesPerView: 6,
 
   });
+
+  const swiper2 = new Swiper('.mySwiper2', {
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.btn-pagination-2',
+      clickable: true,
+      
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.btn-next-2',
+      prevEl: '.btn-prev-2',
+    },
+
+    spaceBetween: 20,
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    slidesPerView: 4,
+
+  });
+
+ 
