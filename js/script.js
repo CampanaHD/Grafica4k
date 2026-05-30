ef21faa326e45dc64a15c00f7c2fc3726fc318e3
Gabriel Campana <gabriel.campana005@gmail.com>
2026-05-29 21:10:28 -0300

    first

--- js/script.js
+++ js/script.js
@@ -91,3 +91,96 @@ function buscar() {
       alert("Digite algo para buscar.");
   }
 }
+
+
+// seção Marcas / empresas
+const logos = [
+
+  [
+    "imagens/Empresas/AGFibra.png",
+    "imagens/Empresas/Amanda Campos.png",
+    "imagens/Empresas/Ana Albanaz.png",
+    "imagens/Empresas/Andaimes Elimar.png",
+    "imagens/Empresas/Apta.png"
+  ],
+
+  [
+    "imagens/Empresas/Art e Estylo.png",
+    "imagens/Empresas/Bem Bolado.png",
+    "imagens/Empresas/Brabus.png",
+    "imagens/Empresas/Carvalima.png",
+    "imagens/Empresas/CTF.png"
+  ],
+
+  [
+    "imagens/Empresas/Espaço Oz.png",
+    "imagens/Empresas/Facil Higiene.png",
+    "imagens/Empresas/GDSul.png",
+    "imagens/Empresas/Havilá.png",
+    "imagens/Empresas/IPM.png"
+  ],
+
+  [
+    "imagens/Empresas/Japapo.png",
+    "imagens/Empresas/Lojas Cem.png",
+    "imagens/Empresas/Los Andes.png",
+    "imagens/Empresas/Magnificos.png",
+    "imagens/Empresas/My Own.png"
+  ],
+
+  [
+    "imagens/Empresas/Naiote.png",
+    "imagens/Empresas/Paraiso das Noivas.png",
+    "imagens/Empresas/Passe Express.png",
+    "imagens/Empresas/Pet Space.png",
+    "imagens/Empresas/Place.png"
+  ],
+
+  [
+    "imagens/Empresas/Potencialize.png",
+    "imagens/Empresas/Pro Saude.png",
+    "imagens/Empresas/RPM.png",
+    "imagens/Empresas/Sabor e Artte.png",
+    "imagens/Empresas/Selfit.png"
+  ],
+
+  [
+    "imagens/Empresas/Triffil.png",
+    "imagens/Empresas/Uniotica.png",
+    "imagens/Empresas/Villa Pet.png",
+    "imagens/Empresas/Ze do Salgado.png",
+    "imagens/Empresas/Zé Branco.png"
+  ]
+
+];
+
+let grupoAtual = 0;
+
+const botao = document.getElementById("trocarLogos");
+const container = document.getElementById("logosContainer");
+
+if (botao && container) {
+
+  botao.addEventListener("click", () => {
+
+    grupoAtual++;
+
+    if (grupoAtual >= logos.length) {
+      grupoAtual = 0;
+    }
+
+    container.classList.add("fade-out");
+
+    setTimeout(() => {
+
+      container.innerHTML = logos[grupoAtual]
+        .map(src => `<img src="${src}" alt="">`)
+        .join("");
+
+      container.classList.remove("fade-out");
+
+    }, 250);
+
+  });
+
+}
